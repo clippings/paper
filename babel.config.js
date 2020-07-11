26a045a87e1bc1b5f4a6e403dabacc083a454774
Evstati Zarkov <evstati.zarkov@gmail.com>
2020-07-11 18:20:06 +0300

    Initial configuration

---
+++ babel.config.js
@@ -0,0 +1,32 @@
+module.exports = function babelConfig(api) {
+  api.cache(true);
+
+  const presets = [
+    [
+      '@babel/env',
+      {
+        useBuiltIns: 'entry',
+        corejs: 3,
+        modules: false,
+      },
+    ],
+    '@babel/react',
+    '@babel/preset-typescript',
+  ];
+
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+  ];
+
+  const env = {
+    test: {
+      presets: ['@babel/env', '@babel/react'],
+    },
+  };
+  return {
+    presets,
+    plugins,
+    env,
+  };
+};
