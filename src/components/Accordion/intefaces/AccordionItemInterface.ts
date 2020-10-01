1ed5702b88613964d4fe026bcdcbaf46685739d3
T3ch0o <32089159+T3ch0o@users.noreply.github.com>
2020-10-01 14:32:15 +0300

    Update src/components/Accordion/intefaces/AccordionItemInterface.ts
    Co-authored-by: MartinAndreev <martin.andreev@clippings.com>

--- src/components/Accordion/intefaces/AccordionItemInterface.ts
+++ src/components/Accordion/intefaces/AccordionItemInterface.ts
@@ -2,6 +2,6 @@ import { ComponentInterface } from '@paper/core/interfaces/ComponentInterface';
 
 export interface AccordionItemInterface extends ComponentInterface {
   onOpen?: () => void;
-  eventKey?: number | any | string;
+  eventKey: any;
   id: number | string;
 }
