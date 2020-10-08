3468eaebfa30660f70c0ed4e84748404b619bf09
T3ch0o <trottimusbg@gmail.com>
2020-10-08 20:00:27 +0300

    [List] Add spacing and className props.

---
+++ src/components/List/List.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import classNames from '@core/config/ClassNames';
+import classnames from 'classnames';
+import { LIST_SPACING } from "@paper/enums";
+import { ListPropsType } from "./types/ListPropsType";
+
+export const List: React.FunctionComponent<ListPropsType> = ({ children = null ,
+                                                               spacing = LIST_SPACING.NONE,
+                                                               className = '' }) => (
+  <div className={classnames(className, classNames.list.container, classNames[spacing])}>{children}</div>
+);
