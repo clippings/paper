fb913527d6d01089ccad653ff5cbf82803334167
T3ch0o <trottimusbg@gmail.com>
2020-10-08 20:52:55 +0300

    [Text] Update Text component and add default type.

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import '../../assets/scss/components.scss';
 import classNames from '@core/config/ClassNames';
 import { TextPropsType } from './types/TextPropsType';
+import { TEXT_TYPE } from '@paper/enums';
 
 export const Text: React.FunctionComponent<TextPropsType> = ({
   children,
-  type,
+  type = TEXT_TYPE.DEFAULT,
   className = '',
-}) => <span className={`${classNames.text[type]} ${className}`}>{children}</span>;
+}) => (
+  <span className={`${classNames.text.container} ${classNames.text[type]} ${className}`}>
+    {children}
+  </span>
+);
