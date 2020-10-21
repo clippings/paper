1e6a76d9734685be73f3845a05a5c221686f938c
Rusi Papazov <rusi@clippings.com>
2020-10-21 13:48:45 +0300

    [TextInput] add forwardRef

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,25 +1,31 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, forwardRef, RefObject } from 'react';
 import classNames from '@core/config/ClassNames';
 import { TextInputPropsType } from './types/TextInputPropsType';
 import { FormGroup } from '../FormGroup/FormGroup';
 
-export const TextInput: React.FunctionComponent<TextInputPropsType> = ({
-  placeholderText,
-  title = null,
-  onChange,
-  error = null,
-  className = '',
-  ...rest
-}) => {
-  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => onChange(e.target.value);
+export const TextInput: React.FunctionComponent<TextInputPropsType> = forwardRef(
+  (
+    { placeholderText, title = null, onChange = null, error = null, className = '', ...rest },
+    ref: RefObject<HTMLInputElement>
+  ) => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+      onChange && onChange(e.target.value);
+    };
 
-  return (
-    <FormGroup className={className} hasError={error !== null}>
-      {title && <span className={classNames.textInput.labelTitle}>{title}</span>}
-      <span className={classNames.textInput.inputContainer}>
-        <input type="text" placeholder={placeholderText} onChange={handleChange} {...rest} />
-      </span>
-      <span className={classNames.textInput.error}>{error}</span>
-    </FormGroup>
-  );
-};
+    return (
+      <FormGroup className={className} hasError={error !== null}>
+        {title && <span className={classNames.textInput.labelTitle}>{title}</span>}
+        <span className={classNames.textInput.inputContainer}>
+          <input
+            ref={ref}
+            type="text"
+            placeholder={placeholderText}
+            onChange={handleChange}
+            {...rest}
+          />
+        </span>
+        <span className={classNames.textInput.error}>{error}</span>
+      </FormGroup>
+    );
+  }
+);
--- src/stories/TextInput/TextInput.stories.tsx
+++ src/stories/TextInput/TextInput.stories.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import figmaDecorator from 'storybook-addon-figma';
 import { storiesOf } from '@storybook/react';
+import { withConsole } from '@storybook/addon-console';
 import { TextInput } from '@paper/components';
 import { jsxDecorator } from 'storybook-addon-jsx';
 import docs from './description.md';
+
+const ref = React.createRef();
 
 const baseProps = {
   title: 'Label',
   placeholderText: 'Placeholder',
   disabled: false,
-  onChange: () => {
-    alert('Detect on change');
+  onChange: value => {
+    console.log(`Value = ${value}`);
+    console.log(ref.current);
   },
 };
-const buildTextInput = props => () => <TextInput {...props} />;
+
+const buildTextInput = props => () => <TextInput {...props} ref={ref} />;
 
 const figmaFile = figmaDecorator({
   url: 'https://www.figma.com/file/9yrktrewqLhb62TIO49Htg/Web-UI-Kit?node-id=142%3A87',
@@ -26,6 +31,7 @@ storiesOf('Inputs', module)
 storiesOf('Inputs', module)
   .addDecorator(jsxDecorator)
   .addDecorator(figmaFile)
+  .addDecorator(withConsole)
   .add(
     'With Error TextInput',
     buildTextInput({
