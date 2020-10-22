140a32451165535407dca81931d11d42c417717d
T3ch0o <trottimusbg@gmail.com>
2020-10-22 15:40:58 +0300

    [ClassNameModifierUtil] Add generateButtonClassName function.

--- src/core/utils/ClassNameModifierUtil.ts
+++ src/core/utils/ClassNameModifierUtil.ts
@@ -1,5 +1,8 @@
+import classNames from 'classnames';
+
 const prefix: string = 'p-';
 const formPrefix: string = 'f-';
+const buttonPrefix = 'uni-button'; // TODO: replace with "button" after buttons-refactoring is complete
 
 /**
  * @param {string} element
@@ -49,3 +52,25 @@ export const createFormElementClassName = (element: string): string =>
  */
 export const createFormElementClassNameModifier = (element: string, modifier: string): string =>
   `${createClassName(formPrefix + element)}__${modifier}`;
+
+export const generateButtonClassName = ({
+                                children,
+                                className,
+                                size,
+                                variant,
+                                shape,
+                                fullWidth,
+                                iconRight,
+                              }: {children: string, className: string, size: string, variant: string, shape: string, fullWidth: string, iconRight: string}): string => {
+  const classes = classNames(prefix, {
+    [`${buttonPrefix}--filled ${prefix}--${variant}`]: variant,
+    [`${buttonPrefix}--${size}`]: size,
+    [`${buttonPrefix}--${shape}`]: shape,
+    [`${buttonPrefix}--full-width`]: fullWidth,
+    [`${buttonPrefix}--icon-right`]: iconRight,
+    [`${buttonPrefix}--empty`]: !children,
+    [className]: className,
+  });
+
+  return classes;
+};
