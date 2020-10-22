d682315ebeab4801f6d65a48c0d09ee8ad8c973f
T3ch0o <trottimusbg@gmail.com>
2020-10-22 16:37:06 +0300

    [DefaultButton] Add component.

---
+++ src/components/Buttons/DefaultButton.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { ButtonPropsType } from './types/ButtonPropsType';
+import { Button } from './Button';
+import { BUTTON_VARIANT } from './enums/ButtonVariantEnum';
+
+export const DefaultButton: React.FunctionComponent<ButtonPropsType> = ({
+  children = null,
+  ...rest
+}) => (
+  <Button variant={BUTTON_VARIANT.DEFAULT} {...rest}>
+    {children}
+  </Button>
+);
