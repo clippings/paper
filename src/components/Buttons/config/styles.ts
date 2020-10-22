4930c804ac801adedebf3095c2325c59d498ae99
T3ch0o <trottimusbg@gmail.com>
2020-10-22 16:31:08 +0300

    [styles] Formatfile.

--- src/components/Buttons/config/styles.ts
+++ src/components/Buttons/config/styles.ts
@@ -1,7 +1,13 @@
 import { generateButtonClassName } from '../../../core/utils/ClassNameModifierUtil';
 
 export const PrimaryButtonStyle: string = generateButtonClassName({ variant: 'primary' });
-export const PrimaryBigButtonStyle: string = generateButtonClassName({ variant: 'primary', size: 'big' });
+export const PrimaryBigButtonStyle: string = generateButtonClassName({
+  variant: 'primary',
+  size: 'big',
+});
 
 export const DefaultButtonStyle: string = generateButtonClassName({ variant: 'default' });
-export const DefaultBigButtonStyle: string = generateButtonClassName({ variant: 'default', size: 'big' });
+export const DefaultBigButtonStyle: string = generateButtonClassName({
+  variant: 'default',
+  size: 'big',
+});
