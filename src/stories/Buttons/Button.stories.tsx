6beeb4d38935bb047ac11c973d54c8c6279bc699
T3ch0o <trottimusbg@gmail.com>
2020-10-23 12:41:43 +0300

    [stories] Update button storybook and scss.

--- src/stories/Buttons/Button.stories.tsx
+++ src/stories/Buttons/Button.stories.tsx
@@ -2,8 +2,8 @@ import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { jsxDecorator } from 'storybook-addon-jsx';
 import figmaDecorator from 'storybook-addon-figma';
-import { Button, CircleButton, DefaultButton, PrimaryButton } from '@paper/components';
-import { BUTTON_SIZE, BUTTON_VARIANT } from '@paper/enums';
+import { Button, CircleButton, DefaultButton, Icon, PrimaryButton } from '@paper/components';
+import { BUTTON_SIZE, BUTTON_VARIANT, ICON, ICONS_COLOR, ICONS_SIZE } from '@paper/enums';
 import { DefaultButtonStyle, PrimaryButtonStyle } from '../../components/Buttons/config/styles';
 
 import '../stories.scss';
@@ -17,155 +17,161 @@ const buttonsFigma = figmaDecorator({
 });
 
 const primBtn = () => (
-  <>
-    <p>
-      <PrimaryButton>I am primary</PrimaryButton>
-    </p>
-    <p>
-      <Button variant={BUTTON_VARIANT.PRIMARY}>I am primary too</Button>
-    </p>
-  </>
+  <div className="buttons-container">
+    <PrimaryButton>I am primary</PrimaryButton>
+    <Button variant={BUTTON_VARIANT.PRIMARY}>I am primary too</Button>
+  </div>
 );
 
 const defBtn = () => (
-  <>
-    <p>
-      <DefaultButton>I am default</DefaultButton>
-    </p>
-    <p>
-      <Button variant={BUTTON_VARIANT.DEFAULT}>I am default too</Button>
-    </p>
-  </>
+  <div className="buttons-container">
+    <DefaultButton>I am default</DefaultButton>
+    <Button variant={BUTTON_VARIANT.DEFAULT}>I am default too</Button>
+  </div>
 );
 
 const circlePrimary = () => <CircleButton variant={BUTTON_VARIANT.PRIMARY}>?</CircleButton>;
 const circleDefault = () => <CircleButton variant={BUTTON_VARIANT.DEFAULT}>?</CircleButton>;
 const circleWithIcon = () => (
-  <CircleButton variant={BUTTON_VARIANT.DEFAULT} className="close-button--example" />
+  <CircleButton variant={BUTTON_VARIANT.DEFAULT}>
+    <Icon icon={ICON.BOARD} color={ICONS_COLOR.DARK} size={ICONS_SIZE.SMALL} />
+  </CircleButton>
 );
 
 const circleShowcase = () => (
   <>
-    <p>
+    <div className="buttons-container">
       <CircleButton variant={BUTTON_VARIANT.PRIMARY}>?</CircleButton>
-    </p>
-    <p>
+    </div>
+    <div className="buttons-container">
       <CircleButton variant={BUTTON_VARIANT.DEFAULT}>?</CircleButton>
-    </p>
-    <p>
-      <CircleButton variant={BUTTON_VARIANT.PRIMARY} className="close-button--example" />
-    </p>
+    </div>
+    <div className="buttons-container">
+      <CircleButton variant={BUTTON_VARIANT.PRIMARY}>
+        <Icon icon={ICON.BOARD} color={ICONS_COLOR.WHITE} size={ICONS_SIZE.SMALL} />
+      </CircleButton>
+    </div>
   </>
 );
 
 const transButtons = () => (
   <>
-    <p>
+    <div className="buttons-container">
       <Button>I am transparent</Button>
-    </p>
-    <p>
-      <Button className="button-happy--example">I am transparent with icon</Button>
-    </p>
-    <p>
-      <Button className="button-happy--example" iconRight>
-        I am transparent with icon on the right
+    </div>
+    <div className="buttons-container">
+      <Button>
+        <Icon icon={ICON.BOARD} color={ICONS_COLOR.DARK} size={ICONS_SIZE.SMALL} /> I am transparent
+        with icon
       </Button>
-    </p>
+    </div>
+    <div className="buttons-container">
+      <Button>
+        I am transparent with icon on the right{' '}
+        <Icon icon={ICON.BOARD} color={ICONS_COLOR.DARK} size={ICONS_SIZE.SMALL} />
+      </Button>
+    </div>
   </>
 );
 
 const iconButtons = () => (
   <>
-    <p className="buttons-container">
-      <PrimaryButton className="button-happy--example">I love you</PrimaryButton>
-      <PrimaryButton className="button-happy--example" iconRight>
-        I love you too
+    <div className="buttons-container">
+      <PrimaryButton>
+        <Icon icon={ICON.BOARD} color={ICONS_COLOR.WHITE} size={ICONS_SIZE.SMALL} /> I love you
       </PrimaryButton>
-      <CircleButton variant={BUTTON_VARIANT.PRIMARY} className="button-happy--example" />
-    </p>
-    <p className="buttons-container">
-      <Button className="button-happy--example">Love yourself</Button>
-      <Button className="button-arrow--example" iconRight>
-        Go forward
+      <PrimaryButton>
+        I love you too <Icon icon={ICON.BOARD} color={ICONS_COLOR.WHITE} size={ICONS_SIZE.SMALL} />
+      </PrimaryButton>
+      <CircleButton variant={BUTTON_VARIANT.PRIMARY}>
+        <Icon icon={ICON.BOARD} color={ICONS_COLOR.WHITE} size={ICONS_SIZE.SMALL} />
+      </CircleButton>
+    </div>
+    <div className="buttons-container">
+      <Button>
+        <Icon icon={ICON.BOARD} color={ICONS_COLOR.DARK} size={ICONS_SIZE.SMALL} /> Love yourself
       </Button>
-    </p>
-    <p className="buttons-container">
-      <DefaultButton className="button-happy--example" fullWidth>
-        Love yourself
+      <Button>
+        Go forward <Icon icon={ICON.BOARD} color={ICONS_COLOR.DARK} size={ICONS_SIZE.SMALL} />
+      </Button>
+    </div>
+    <div className="buttons-container">
+      <DefaultButton fullWidth>
+        <Icon icon={ICON.BOARD} color={ICONS_COLOR.DARK} size={ICONS_SIZE.SMALL} /> Love yourself
       </DefaultButton>
-    </p>
+    </div>
   </>
 );
 
 const buttonLike = () => (
   <>
-    <p>
+    <div className="buttons-container">
       <a href="http://google.com" className={PrimaryButtonStyle}>
         I am a link that looks like a button
       </a>
-    </p>
-    <p>
+    </div>
+    <div className="buttons-container">
       <a href="http://google.com" className={DefaultButtonStyle}>
         I am a link that looks like a button
       </a>
-    </p>
+    </div>
   </>
 );
 
 const showcase = () => (
   <>
-    <p className="buttons-container">
+    <div className="buttons-container">
       <PrimaryButton size={BUTTON_SIZE.BIG}>Big button</PrimaryButton>
       <PrimaryButton>Medium button</PrimaryButton>
       <PrimaryButton size={BUTTON_SIZE.SMALL}>Small button</PrimaryButton>
-    </p>
-    <p className="buttons-container">
+    </div>
+    <div className="buttons-container">
       <DefaultButton size={BUTTON_SIZE.BIG}>Big button</DefaultButton>
       <DefaultButton>Medium button</DefaultButton>
       <DefaultButton size={BUTTON_SIZE.SMALL}>Small button</DefaultButton>
-    </p>
-    <p className="buttons-container">
+    </div>
+    <div className="buttons-container">
       <CircleButton variant={BUTTON_VARIANT.PRIMARY}>?</CircleButton>
       <CircleButton variant={BUTTON_VARIANT.DEFAULT}>!</CircleButton>
-      <CircleButton variant={BUTTON_VARIANT.PRIMARY} className="button-happy--example" />
-    </p>
-    <p className="buttons-container">
-      <CircleButton
-        variant={BUTTON_VARIANT.DEFAULT}
-        size={BUTTON_SIZE.BIG}
-        className="close-button--example"
-      />
-      <CircleButton variant={BUTTON_VARIANT.DEFAULT} className="close-button--example" />
-      <CircleButton
-        variant={BUTTON_VARIANT.DEFAULT}
-        size={BUTTON_SIZE.SMALL}
-        className="close-button--example"
-      />
-    </p>
-    <p>
+      <CircleButton variant={BUTTON_VARIANT.PRIMARY}>
+        <Icon icon={ICON.BOARD} color={ICONS_COLOR.WHITE} size={ICONS_SIZE.SMALL} />
+      </CircleButton>
+    </div>
+    <div className="buttons-container">
+      <CircleButton variant={BUTTON_VARIANT.DEFAULT} size={BUTTON_SIZE.BIG}>
+        <Icon icon={ICON.BOARD} color={ICONS_COLOR.DARK} size={ICONS_SIZE.LARGE} />
+      </CircleButton>
+      <CircleButton variant={BUTTON_VARIANT.DEFAULT}>
+        <Icon icon={ICON.BOARD} color={ICONS_COLOR.DARK} size={ICONS_SIZE.MEDIUM} />
+      </CircleButton>
+      <CircleButton variant={BUTTON_VARIANT.DEFAULT} size={BUTTON_SIZE.SMALL}>
+        <Icon icon={ICON.BOARD} color={ICONS_COLOR.DARK} size={ICONS_SIZE.SMALL} />
+      </CircleButton>
+    </div>
+    <div className="buttons-container">
       <PrimaryButton fullWidth>Full-width (100%) button</PrimaryButton>
-    </p>
-    <p>
+    </div>
+    <div className="buttons-container">
       <DefaultButton fullWidth>Full-width (100%) button</DefaultButton>
-    </p>
-    <p>
+    </div>
+    <div className="buttons-container">
       <a href="http://google.com" className={PrimaryButtonStyle}>
         I am a link that looks like a button
       </a>
-    </p>
-    <p>
+    </div>
+    <div className="buttons-container">
       <a href="http://google.com" className={DefaultButtonStyle}>
         I am a link that looks like a button
       </a>
-    </p>
+    </div>
   </>
 );
 
 const sm = () => (
-  <p className="buttons-container">
+  <div className="buttons-container">
     <Button className="button-twitter">twitter button</Button>
     <Button className="button-pinterest">pinterest button</Button>
-  </p>
+  </div>
 );
 
 storiesOf('Buttons', module)
