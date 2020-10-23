b99b14cf6defa284fafcaef9edf205c6515b5101
T3ch0o <trottimusbg@gmail.com>
2020-10-23 12:40:42 +0300

    [CircleButton] Add children inside body.

--- src/components/Buttons/CircleButton.tsx
+++ src/components/Buttons/CircleButton.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import { Button } from './Button';
 import { BUTTON_SHAPE } from '@paper/enums';
-import { CircleButtonPropsType } from './types/CircleButtonPropsType';
+import { ButtonPropsType } from './types/ButtonPropsType';
 
-export const CircleButton: React.FunctionComponent<CircleButtonPropsType> = ({ ...rest }) => (
-  <Button shape={BUTTON_SHAPE.CIRCLE} {...rest} />
+export const CircleButton: React.FunctionComponent<ButtonPropsType> = ({
+  children = null,
+  ...rest
+}) => (
+  <Button shape={BUTTON_SHAPE.CIRCLE} {...rest}>
+    {children}
+  </Button>
 );
