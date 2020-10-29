76cc18c49584755779f383b61c0b2ed40eb859b8
T3ch0o <trottimusbg@gmail.com>
2020-10-29 12:50:01 +0200

    [ButtonProps] Remove shape.

--- src/components/Buttons/types/ButtonPropsType.ts
+++ src/components/Buttons/types/ButtonPropsType.ts
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import { BUTTON_SIZE, BUTTON_SHAPE, BUTTON_VARIANT, BUTTON_TYPE } from '@paper/enums';
+import { BUTTON_SIZE, BUTTON_VARIANT, BUTTON_TYPE } from '@paper/enums';
 
 export type ButtonPropsType = {
   children: React.ReactNode;
   className?: string;
   size?: BUTTON_SIZE;
-  shape?: BUTTON_SHAPE;
   variant?: BUTTON_VARIANT;
   fullWidth?: boolean;
   type?: BUTTON_TYPE;
