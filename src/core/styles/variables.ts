8e70985648c57412dc0a58af20f74a29e678682c
T3ch0o <trottimusbg@gmail.com>
2020-10-30 18:25:21 +0200

    [variables] Add HM colours and onPrimary/background.

--- src/core/styles/variables.ts
+++ src/core/styles/variables.ts
@@ -1,13 +1,14 @@
 import theme from 'styled-theming';
 
-// Primitive light theme colors
-const graphite120 = '#141414';
+// Primitive theme colors
+const graphite120 = { light: '#141414', hm: '#111111' };
 const graphite90 = '#525252';
 const graphite60 = '#8f8f8f';
 const graphite30 = '#cccccc';
 const graphite10 = '#f5f5f5';
-export const white = '#ffffff';
+const white = '#ffffff';
 
+const hmRed = '#D13F21';
 const red60 = '#d74b46';
 const yellow50 = '#f1b056';
 const green30 = '#4aab4a';
@@ -17,16 +18,19 @@ const beige10 = '#f3ede9';
 export const twitterColor = '#03aaf5';
 export const pinterestColor = '#d02621';
 
-// Abstract light theme colors
-export const brandPrimary = theme('mode', { light: graphite120 });
-export const darkest = theme('mode', { light: graphite120 });
-export const darker = theme('mode', { light: graphite90 });
-export const medium = theme('mode', { light: graphite60 });
-export const light = theme('mode', { light: graphite30 });
-export const lightest = theme('mode', { light: graphite10 });
+// Abstract theme colors
+export const darkest = theme('mode', graphite120);
+export const darker = theme('mode', { light: graphite90, hm: graphite90 });
+export const medium = theme('mode', { light: graphite60, hm: graphite60 });
+export const light = theme('mode', { light: graphite30, hm: graphite30 });
+export const lightest = theme('mode', { light: graphite10, hm: graphite10 });
 
-export const attention = theme('mode', { light: red60 });
-export const caution = theme('mode', { light: yellow50 });
-export const success = theme('mode', { light: green30 });
-export const information = theme('mode', { light: blue50 });
-export const brandBackground = theme('mode', { light: beige10 });
+export const brandPrimary = theme('mode', { light: graphite120.light, hm: hmRed });
+export const brandBackground = theme('mode', { light: beige10, hm: beige10 });
+export const onPrimary = theme('mode', { light: white, hm: white });
+export const background = theme('mode', { light: white, hm: white });
+
+export const attention = theme('mode', { light: red60, hm: red60 });
+export const caution = theme('mode', { light: yellow50, hm: yellow50 });
+export const success = theme('mode', { light: green30, hm: green30 });
+export const information = theme('mode', { light: blue50, hm: blue50 });
