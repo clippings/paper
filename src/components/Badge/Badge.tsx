96bc965a43f74805b942df8a04fe74eff4e5c92a
Maria <maria@clippings.com>
2020-12-21 14:10:49 +0200

    Add ref to badge component

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { forwardRef, RefObject } from 'react';
 import { BADGE_TYPE } from './enums/BadgeTypeEnum';
 import { BADGE_SIZE } from './enums/BadgeSizeEnum';
 import classNames from '@core/config/ClassNames';
 import { BadgeInterface } from './types/BagdePropsType';
 
-export const Badge: React.FunctionComponent<BadgeInterface> = ({
-  className = '',
-  type = BADGE_TYPE.UNSPECIFIED,
-  size = BADGE_SIZE.SMALL,
-  children = null,
-}) => (
-  <span className={`${classNames.badge[type]} ${classNames.badge[size]} ${className}`}>
-    {children}
-  </span>
+export const Badge: React.FunctionComponent<BadgeInterface> = forwardRef(
+  (
+    {
+      className = '',
+      type = BADGE_TYPE.UNSPECIFIED,
+      size = BADGE_SIZE.SMALL,
+      children = null,
+      ...rest
+    },
+    ref: RefObject<HTMLInputElement>
+  ) => (
+    <span
+      ref={ref}
+      className={`${classNames.badge[type]} ${classNames.badge[size]} ${className}`}
+      {...rest}
+    >
+      <span>Badge</span>
+      {children}
+    </span>
+  )
 );
