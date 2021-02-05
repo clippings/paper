28c3b03be1541469cafaf00539d57d29b9c8383e
Maria <maria@clippings.com>
2021-02-05 16:18:59 +0200

    Adding new stories using mdx

---
+++ src/stories/Buttons/ButtonsExample.tsx
@@ -0,0 +1,39 @@
+import React, { FC } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Button, CircleButton } from '@paper/components';
+
+import { ButtonPropsType } from '../../components/Buttons/types/ButtonPropsType';
+
+const ButtonRegularExample: FC<ButtonPropsType & { theme: string }> = ({
+  theme,
+  variant,
+  size,
+  children,
+  ...rest
+}) => {
+  return (
+    <ThemeProvider theme={{ mode: theme }}>
+      <Button variant={variant} size={size} {...rest}>
+        {children}
+      </Button>
+    </ThemeProvider>
+  );
+};
+
+const CircleButtonExample: FC<ButtonPropsType & { theme: string }> = ({
+  theme,
+  variant,
+  size,
+  children,
+  ...rest
+}) => {
+  return (
+    <ThemeProvider theme={{ mode: theme }}>
+      <CircleButton variant={variant} size={size} {...rest}>
+        {children}
+      </CircleButton>
+    </ThemeProvider>
+  );
+};
+
+export { ButtonRegularExample, CircleButtonExample };
