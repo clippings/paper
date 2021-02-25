56a209ded8d57c689098527f265c84d026113db8
Nikolay Ivanov <nikolay.ivanov321@gmail.com>
2021-02-25 17:00:47 +0200

    export registerLocale and setDefaultLocale from react datepicker

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import './assets/scss/components.scss';
 import './assets/scss/forms.scss';
 
+export { registerLocale, setDefaultLocale } from 'react-datepicker';
 export { ThemeProvider, ServerStyleSheet } from 'styled-components';
 export * from './index.components';
 export * from './index.enums';
