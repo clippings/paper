d6947ce51168ddfc388306d41a280230d0e35acc
Maria <maria@clippings.com>
2021-03-21 19:46:40 +0200

    Migrate Card stories to mdx

--- src/stories/CardGrid/gridCardUtild.tsx
+++ src/stories/CardGrid/gridCardUtild.tsx
@@ -1,21 +1,6 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { Card } from '@paper/components';
-import { jsxDecorator } from 'storybook-addon-jsx';
 import styled from 'styled-components';
 
-const CardGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 16px;
-  grid-auto-rows: minmax(100px, auto);
-  @media only screen and (min-width: 768px) {
-    grid-template-columns: repeat(3, 1fr);
-    gap: 16px 8px;
-  }
-`;
-
-const items = [
+export const gridCardUtild = [
   {
     key: 'key-0',
     img: 'https://res.cloudinary.com/clippings/image/upload/v1611495053/placeholders/image-n-a.png',
@@ -42,7 +27,7 @@ const items = [
     description: { primary: 'A03 Light-blue', secondary: null },
   },
   {
-    key: 'key-4',
+    key: 'key-5',
     img:
       'https://res.cloudinary.com/clippings/image/upload/w_150,ar_1:1,c_fill,g_auto,e_art:hokusai/v1554271662/A03%20Upholstery/A03_Blue.jpg',
     description: { primary: 'A03 Blue', secondary: null },
@@ -54,14 +39,14 @@ const items = [
     description: { primary: 'A03 Dark-grey', secondary: null },
   },
 ];
-const buildItemGrid = () => (
-  <div style={{ maxWidth: '500px', padding: '24px', backgroundColor: '#e5e5e5' }}>
-    <CardGrid>
-      {items.map(item => (
-        <Card {...item} selected={item.key === 'key-1'} key={item.key} />
-      ))}
-    </CardGrid>
-  </div>
-);
 
-storiesOf('Card', module).addDecorator(jsxDecorator).add('Grid example', buildItemGrid);
+export const CardGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 16px;
+  grid-auto-rows: minmax(100px, auto);
+  @media only screen and (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+    gap: 16px 8px;
+  }
+`;
