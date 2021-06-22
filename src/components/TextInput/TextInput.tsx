fd8624fa0aa0b2232e50df39d2d95811b8d707cf
Maria <maria@clippings.com>
2021-06-22 15:18:35 +0300

    Make TextInput component based on field component

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,41 +1,9 @@
 import React, { forwardRef, RefObject } from 'react';
-import classNames from '@core/config/ClassNames';
 import { TextInputPropsType } from './types/TextInputPropsType';
-import { FormGroup } from '../FormGroup/FormGroup';
-import { FormError } from '../FormError/FormError';
-import { Label } from '../Label/Label';
-import { handleChange } from '@core/utils/InputUtils';
-import classnames from 'classnames';
+import { InputField } from '../InputField/InputField';
 
 export const TextInput: React.FunctionComponent<TextInputPropsType> = forwardRef(
-  (
-    {
-      id = undefined,
-      placeholderText = '',
-      title = null,
-      onChange,
-      error = null,
-      className = '',
-      ...rest
-    },
-    ref: RefObject<HTMLInputElement>
-  ) => {
-    return (
-      <FormGroup className={className}>
-        {title && <Label htmlFor={id}>{title}</Label>}
-        <input
-          id={id}
-          ref={ref}
-          type="text"
-          placeholder={placeholderText}
-          className={classnames(classNames.textInput.input, {
-            [classNames.formGroup.hasError]: error !== null,
-          })}
-          onChange={handleChange(onChange)}
-          {...rest}
-        />
-        {error && <FormError>{error}</FormError>}
-      </FormGroup>
-    );
+  ({ ...rest }, ref: RefObject<HTMLInputElement>) => {
+    return <InputField ref={ref} type="text" {...rest} />;
   }
 );
